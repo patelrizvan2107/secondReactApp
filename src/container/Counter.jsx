54e51445cf04cad0e6725f4d1fb4fd90d06d0926
patelrizvan2107 <patelrizvan2107@gmail.com>
2026-08-07 15:33:29 +0530

    toggle&task

---
+++ src/container/Counter.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function Counter(props) {
+  const [count, setCount] = useState(0);
+
+  console.log(props);
+  
+  const inc = () => {
+    console.log("hiii");
+    setCount(count + 1);
+  };
+
+  const dec = () => {
+    if (count < 1) {
+      alert("enough ok");
+    } else {
+      setCount(count - 1);
+    }
+  };
+  return (
+    <div>
+      <button onClick={() =>setCount((count) => count - 1)} disabled={count===0}>-</button>
+      {count}
+      <button onClick={() => inc()} disabled={count===5}>+</button>
+    </div>
+  );
+}
+
+export default Counter;
---
+++ src/container/Password.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
+
+
+
+function Password(props) {
+   
+    const [toggle, setToggle] = useState(true)
+
+    return (
+        <div>
+           <input type={toggle ? 'password' : 'text'} placeholder='Enter PassWord' />
+           <button onClick={() => setToggle((toggle) => !toggle) }>{
+            
+            !toggle ? <FaEyeSlash/> : <FaEye /> 
+            
+            
+            }</button>
+        </div>
+    );
+}
+
+export default Password;
+
+//dropdown      Country         My country is: India
