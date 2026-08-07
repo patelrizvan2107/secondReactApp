4c988a303d8e12800a47c96865e48e6bca981b7f
patelrizvan2107 <patelrizvan2107@gmail.com>
2026-08-07 14:01:22 +0530

    sdsd

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>Lets Get Party started</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
@@ -24,7 +24,7 @@ function App() {
         <button
           type="button"
           className="counter"
-          onClick={() => setCount((count) => count + 1)}
+          onClick={() => setCount((c) => c + 1)}
         >
           Count is {count}
         </button>
